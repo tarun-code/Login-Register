ce52356e5f212e274301d21ea283f55fc6d8dd1d
tarun Shori <tarunshori801@gmail.com>
2023-03-24 23:38:20 +0530

    Initial commit

---
+++ Login&Register/backend/index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/myLoginRegisterDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("DB connected");
+  }
+);
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
+
+//Routes
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      if (password === user.password) {
+        res.send({ message: "Login Successfull", user: user });
+      } else {
+        res.send({ message: "Password Didn't Match" });
+      }
+    } else {
+      res.send({ message: "User Not Registered" });
+    }
+  });
+});
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      res.send({ message: "User Already Registerd" });
+    } else {
+      const user = new User({
+        name,
+        email,
+        password,
+      });
+      user.save((err) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send({ message: "Successfully Registered, Please Login Now." });
+        }
+      });
+    }
+  });
+});
+
+app.listen(4000, () => {
+  console.log(" started at port 4000");
+});
